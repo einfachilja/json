95807f596470041a9908eb31f9b2715c0173a2ae
Ilja Savkiv <einfachilja@iMac.fritz.box>
2025-04-25 18:06:20 +0200

    added JSONArray filter()-Method

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-
+// 06 - JSON vs. Object
 // kein valides JSON, sondern Object
+/*
 let myObject = {
     "name": "Ilja",
     job : function () {
@@ -17,3 +18,45 @@ let mySJON = {
 };
 
 console.table(mySJON)
+*/
+// 07 - JSONArray: filter-Methode
+let myArray = [
+    {
+        "name": "Ilja",
+        "good_guy": true,
+        "age": 25,
+        'of_legal_age': true
+    },
+    {
+        "name": "Michael",
+        "good_guy": true,
+        "age": 16,
+        'of_legal_age': false
+    },
+    {
+        "name": "Daniel",
+        "good_guy": false,
+        "age": 17,
+        'of_legal_age': false
+    },
+    {
+        "name": "Jannik",
+        "good_guy": true,
+        "age": 24,
+        'of_legal_age': true
+    },
+    {
+        "name": "Arne",
+        "good_guy": false,
+        "age": 19,
+        'of_legal_age': true
+    }
+];
+
+let result = myArray.filter((element) => {return element['good_guy'] == true});
+console.table(result);
+
+let old = myArray.filter((element) => {return element['age'] < 18});
+console.table(old);
+
+
